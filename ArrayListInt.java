79ef203074b1e0c06e7546ffda4c8793f1c48fa8
Sourdaci <sourdaci@gmail.com>
2015-02-20 14:16:52 +0100

    metodo add(int, int) no redundante

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -34,22 +34,16 @@ public class ArrayListInt
     public void add(int posicion, int valor){
         int longitudBase = arrayDeEnteros.length; // Guardo tamaño
         if (posicion >= 0 && posicion < longitudBase){ // Si la posicion esta en rango y hay elementos
-            // Esta comprobacion no funciona con 0 y 0, resuelto en el else
-            if(posicion < longitudBase){
-                int[] arrayTemp = new int[longitudBase + 1]; // Creo el nuevo array
-                for (int i1 = 0; i1 < posicion; i1++){ // Guardo datos hasta llegar a posicion
-                    arrayTemp[i1] = arrayDeEnteros[i1];
-                }
-                arrayTemp[posicion] = valor; // Guardo el nuevo dato en posicion
-                for (int i2 = posicion + 1; i2 < longitudBase + 1; i2++){
-                    // Guardo los siguientes datos hasta acabar
-                    arrayTemp[i2] = arrayDeEnteros[i2 - 1];
-                }
-                arrayDeEnteros = arrayTemp; // Reasigno las referencias para quedarme con el array bueno
+            int[] arrayTemp = new int[longitudBase + 1]; // Creo el nuevo array
+            for (int i1 = 0; i1 < posicion; i1++){ // Guardo datos hasta llegar a posicion
+                arrayTemp[i1] = arrayDeEnteros[i1];
             }
-        }else if (posicion == 0 && longitudBase == 0){
-            // Si el array esta vacio y la posicion es cero, llama al otro metodo add
-            add(valor);
+            arrayTemp[posicion] = valor; // Guardo el nuevo dato en posicion
+            for (int i2 = posicion + 1; i2 < longitudBase + 1; i2++){
+                // Guardo los siguientes datos hasta acabar
+                arrayTemp[i2] = arrayDeEnteros[i2 - 1];
+            }
+            arrayDeEnteros = arrayTemp; // Reasigno las referencias para quedarme con el array bueno
         }
     }
     
